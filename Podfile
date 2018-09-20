892edeb1ccabfa64eb4ce692e9395a81cce5d2f1
Romain Pouclet <romain.pouclet@gmail.com>
2018-09-20 10:36:23 -0700

    Initial commit

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SampleAppWithCheckedInPodsButNotAllOfThem' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'ORStackView', '2.0'
+  pod 'FLKAutoLayout', '0.1.1'
+  pod 'ARCollectionViewMasonryLayout', '~> 2.0.0'
+  pod 'SDWebImage', '~> 3.7'
+  pod 'SVProgressHUD'
+end
---
+++ Pods/Target Support Files/FLKAutoLayout/FLKAutoLayout-umbrella.h
@@ -0,0 +1,19 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "FLKAutoLayoutPredicateList.h"
+#import "UIView+FLKAutoLayout.h"
+#import "UIView+FLKAutoLayoutPredicate.h"
+
+FOUNDATION_EXPORT double FLKAutoLayoutVersionNumber;
+FOUNDATION_EXPORT const unsigned char FLKAutoLayoutVersionString[];
+
